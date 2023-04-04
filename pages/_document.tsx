9eaca8e0bda5c23e6e536d6f5b92e6ccd642b4fe
dasu.rahul <dasu.rahul@kellton.com>
2023-04-04 11:12:06 +0530

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,19 +4,7 @@ export default function Document() {
   const VERCEL_ID = "prj_90eCgTbr4nhE0O3vhJeG57RWCbcS";
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src={`https://vitals.vercel-insights.com/cdn/vercel-insights.js?v=${Date.now()}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.vercelId='${VERCEL_ID}';
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
